d8b0c71dea00868a9b5c44ff869cdc971e558e3d
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2024-01-25 17:19:13 +0700

    feat: retrieval chain

--- fact_app/prompt.py
+++ fact_app/prompt.py
@@ -1,11 +1,26 @@
 from dotenv import load_dotenv
 from langchain_community.vectorstores.chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import RetrievalQA
+
 load_dotenv()
 
+chat = ChatOpenAI()
 embeddings = OpenAIEmbeddings()
-
 db = Chroma(
     embedding_function=embeddings,
     persist_directory="chroma/factos"
 )
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff"
+)
+
+result = chain.invoke({
+    "query":  "What is an interesting fact about the English language?"
+})
+
+print(result)
